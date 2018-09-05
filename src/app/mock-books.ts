c73bc9ef5ee6d2cb260b0509d325ea7fca88d6db
yAshitaba <tianlin0515@gmail.com>
2018-09-05 13:19:40 +0900

    [change]replace broken book images for mock-books

--- src/app/mock-books.ts
+++ src/app/mock-books.ts
@@ -3,7 +3,7 @@ export const BOOKS: Book[] = [
     { book_id: 11, cover_image_url: 'HTML5_level1.jpg' },
     { book_id: 12, cover_image_url: 'HTML5_level2.jpg' },
     { book_id: 13, cover_image_url: 'HTML5_beginner.jpg' },
-    { book_id: 14, cover_image_url: 'java_begginer.jpg' },
+    { book_id: 14, cover_image_url: 'java_beginner.jpg' },
     { book_id: 15, cover_image_url: 'java_developer.jpg' },
     { book_id: 16, cover_image_url: 'java_serous.jpg' },
     { book_id: 17, cover_image_url: 'javase_bronze.jpg' },
