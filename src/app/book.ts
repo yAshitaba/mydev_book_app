ab5368728d25af9f898123651f036acbec6c65dc
yAshitaba <tianlin0515@gmail.com>
2018-09-05 12:45:30 +0900

    [add]book class and mock-books data

---
+++ src/app/book.ts
@@ -0,0 +1,18 @@
+export class Book {
+    book_id: number;
+    //title: string;
+    //author: string;
+    //publisher: string;
+    //published_date: string;
+    //isbn: string;
+    //category_id: number;
+    //rate: number;
+    //page_url: string;
+    cover_image_url: string;
+    //memo: string;
+    //delete_flg: string;
+
+}
+// function getPublishedDate() {
+//     return new Date(this.published_date);
+// }
---
+++ src/app/mock-books.ts
@@ -0,0 +1,13 @@
+import { Book } from './book';
+export const BOOKS: Book[] = [
+    { book_id: 11, cover_image_url: 'Mr. Nice' },
+    { book_id: 12, cover_image_url: 'Narco' },
+    { book_id: 13, cover_image_url: 'Bombasto' },
+    { book_id: 14, cover_image_url: 'Celeritas' },
+    { book_id: 15, cover_image_url: 'Magneta' },
+    { book_id: 16, cover_image_url: 'RubberMan' },
+    { book_id: 17, cover_image_url: 'Dynama' },
+    { book_id: 18, cover_image_url: 'Dr IQ' },
+    { book_id: 19, cover_image_url: 'Magma' },
+    { book_id: 20, cover_image_url: 'Tornado' }
+];
