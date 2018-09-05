760572646a3bb4666f23f1df30c4622b559b4ab5
yAshitaba <tianlin0515@gmail.com>
2018-09-06 00:20:57 +0900

    [fix]book-list.html error & enable ios emulator

--- src/app/mock-books.ts
+++ src/app/mock-books.ts
@@ -1,15 +1,88 @@
 import { Book } from './book';
 export const BOOKS: Book[] = [
-    { book_id: 11, cover_image_url: 'HTML5_level1.jpg' },
-    { book_id: 12, cover_image_url: 'HTML5_level2.jpg' },
-    { book_id: 13, cover_image_url: 'HTML5_beginner.jpg' },
-    { book_id: 14, cover_image_url: 'java_beginner.jpg' },
-    { book_id: 15, cover_image_url: 'java_developer.jpg' },
-    { book_id: 16, cover_image_url: 'java_serious.jpg' },
-    { book_id: 17, cover_image_url: 'javase_bronze.jpg' },
-    { book_id: 18, cover_image_url: 'servlet_jsp.jpg' },
-    { book_id: 19, cover_image_url: 'javaee_websystem.jpg' },
-    { book_id: 20, cover_image_url: 'design_pattern.jpg' },
-    { book_id: 21, cover_image_url: 'effective_java2.jpg' },
-    { book_id: 22, cover_image_url: 'ionic_mobile_apps.jpg' }
+    {
+        // HTML5 level1
+        book_id: 11, title: null, author: null, publisher: null, published_date: null,
+        isbn: null, category_id: null, rate: null, page_url: null, memo: null, delete_flg: null,
+        cover_image_url: 'https://images-na.ssl-images-amazon.com/images/I/51enLiEOnXL._SX387_BO1,204,203,200_.jpg'
+    },
+    {
+        // HTML5 level2
+        book_id: 12, title: null, author: null, publisher: null, published_date: null,
+        isbn: null, category_id: null, rate: null, page_url: null, memo: null, delete_flg: null,
+        cover_image_url: 'https://images-na.ssl-images-amazon.com/images/I/51As2xmMxHL._SX386_BO1,204,203,200_.jpg'
+    },
+    {
+        // 入門HTML5
+        book_id: 13, title: null, author: null, publisher: null, published_date: null,
+        isbn: null, category_id: null, rate: null, page_url: null, memo: null, delete_flg: null,
+        cover_image_url: 'https://images-na.ssl-images-amazon.com/images/I/51uOOBdPsTL._SX389_BO1,204,203,200_.jpg'
+    },
+    {
+        // スッキリわかるJava入門
+        book_id: 14, title: null, author: null, publisher: null, published_date: null,
+        isbn: null, category_id: null, rate: null, page_url: null, memo: null, delete_flg: null,
+        cover_image_url: 'https://images-na.ssl-images-amazon.com/images/I/51p6dqvB3lL._SX351_BO1,204,203,200_.jpg'
+    },
+    {
+        // スッキリわかるJava入門実践編
+        book_id: 15, title: null, author: null, publisher: null, published_date: null,
+        isbn: null, category_id: null, rate: null, page_url: null, memo: null, delete_flg: null,
+        cover_image_url: 'https://images-na.ssl-images-amazon.com/images/I/51tln1rTTnL._SX351_BO1,204,203,200_.jpg'
+    },
+    {
+        // Java本格入門
+        book_id: 16, title: null, author: null, publisher: null, published_date: null,
+        isbn: null, category_id: null, rate: null, page_url: null, memo: null, delete_flg: null,
+        cover_image_url: 'https://images-na.ssl-images-amazon.com/images/I/51741IwOl5L._SX394_BO1,204,203,200_.jpg'
+    },
+    {
+        // パーフェクトJava
+        book_id: 17, title: null, author: null, publisher: null, published_date: null,
+        isbn: null, category_id: null, rate: null, page_url: null, memo: null, delete_flg: null,
+        cover_image_url: 'https://images-na.ssl-images-amazon.com/images/I/51Or5uklizL._SX393_BO1,204,203,200_.jpg'
+    },
+    {
+        // Effective Java 第二版
+        book_id: 18, title: null, author: null, publisher: null, published_date: null,
+        isbn: null, category_id: null, rate: null, page_url: null, memo: null, delete_flg: null,
+        cover_image_url: 'https://images-na.ssl-images-amazon.com/images/I/51lEBnUjJqL._SX387_BO1,204,203,200_.jpg'
+    },
+    {
+        // デザインパターン入門
+        book_id: 19, title: null, author: null, publisher: null, published_date: null,
+        isbn: null, category_id: null, rate: null, page_url: null, memo: null, delete_flg: null,
+        cover_image_url: 'https://images-na.ssl-images-amazon.com/images/I/51QsmvkObML._SX392_BO1,204,203,200_.jpg'
+    },
+    {
+        // サーブレット＆JSP入門
+        book_id: 20, title: null, author: null, publisher: null, published_date: null,
+        isbn: null, category_id: null, rate: null, page_url: null, memo: null, delete_flg: null,
+        cover_image_url: 'https://images-na.ssl-images-amazon.com/images/I/51XPa8DJ3cL._SX350_BO1,204,203,200_.jpg'
+    },
+    {
+        // SPRING徹底入門
+        book_id: 21, title: null, author: null, publisher: null, published_date: null,
+        isbn: null, category_id: null, rate: null, page_url: null, memo: null, delete_flg: null,
+        cover_image_url: 'https://images-na.ssl-images-amazon.com/images/I/6160VVxyCcL._SX394_BO1,204,203,200_.jpg'
+    },
+    {
+        // ionic モバイルアプリ入門
+        book_id: 22, title: null, author: null, publisher: null, published_date: null,
+        isbn: null, category_id: null, rate: null, page_url: null, memo: null, delete_flg: null,
+        cover_image_url: 'https://images-na.ssl-images-amazon.com/images/I/51StnIanx1L._SX350_BO1,204,203,200_.jpg'
+    }
+
+    // { book_id: 11, cover_image_url: 'HTML5_level1.jpg' },
+    // { book_id: 12, cover_image_url: 'HTML5_level2.jpg' },
+    // { book_id: 13, cover_image_url: 'HTML5_beginner.jpg' },
+    // { book_id: 14, cover_image_url: 'java_beginner.jpg' },
+    // { book_id: 15, cover_image_url: 'java_developer.jpg' },
+    // { book_id: 16, cover_image_url: 'java_serious.jpg' },
+    // { book_id: 17, cover_image_url: 'javase_bronze.jpg' },
+    // { book_id: 18, cover_image_url: 'servlet_jsp.jpg' },
+    // { book_id: 19, cover_image_url: 'javaee_websystem.jpg' },
+    // { book_id: 20, cover_image_url: 'design_pattern.jpg' },
+    // { book_id: 21, cover_image_url: 'effective_java2.jpg' },
+    // { book_id: 22, cover_image_url: 'ionic_mobile_apps.jpg' }
 ];
