8143a2a1dc4715468cf75bef34b97d749be61e73
yAshitaba <tianlin0515@gmail.com>
2018-09-05 13:03:51 +0900

    [add]book images for mock-books

--- src/app/mock-books.ts
+++ src/app/mock-books.ts
@@ -1,13 +1,15 @@
 import { Book } from './book';
 export const BOOKS: Book[] = [
-    { book_id: 11, cover_image_url: 'Mr. Nice' },
-    { book_id: 12, cover_image_url: 'Narco' },
-    { book_id: 13, cover_image_url: 'Bombasto' },
-    { book_id: 14, cover_image_url: 'Celeritas' },
-    { book_id: 15, cover_image_url: 'Magneta' },
-    { book_id: 16, cover_image_url: 'RubberMan' },
-    { book_id: 17, cover_image_url: 'Dynama' },
-    { book_id: 18, cover_image_url: 'Dr IQ' },
-    { book_id: 19, cover_image_url: 'Magma' },
-    { book_id: 20, cover_image_url: 'Tornado' }
+    { book_id: 11, cover_image_url: 'HTML5_level1.jpg' },
+    { book_id: 12, cover_image_url: 'HTML5_level2.jpg' },
+    { book_id: 13, cover_image_url: 'HTML5_beginner.jpg' },
+    { book_id: 14, cover_image_url: 'java_begginer.jpg' },
+    { book_id: 15, cover_image_url: 'java_developer.jpg' },
+    { book_id: 16, cover_image_url: 'java_serous.jpg' },
+    { book_id: 17, cover_image_url: 'javase_bronze.jpg' },
+    { book_id: 18, cover_image_url: 'servlet_jsp.jpg' },
+    { book_id: 19, cover_image_url: 'javaee_websystem.jpg' },
+    { book_id: 20, cover_image_url: 'design_pattern.jpg' },
+    { book_id: 21, cover_image_url: 'effective_java2.jpg' },
+    { book_id: 22, cover_image_url: 'ionic_mobile_apps.jpg' }
 ];
